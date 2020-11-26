43ad295351c53ca5b9092baffcdd4b713873c3b7
Matteo Schiavon <matteoschiav@yahoo.it>
2020-11-26 21:09:11 +0100

    First implementation of the model

--- netsquid_freespace/lossmodel.py
+++ netsquid_freespace/lossmodel.py
@@ -12,12 +12,29 @@ This file contains the different loss models implemented for the free space chan
 import numpy as np
 from scipy.special import i0, i1
 
-import netsquid as ns
 from netsquid.components.models.qerrormodels import QuantumErrorModel
 import netsquid.util.simtools as simtools
 from netsquid.util.simlog import warn_deprecated
 
-
+def randomSample(pdf,xmin=0,xmax=1,rng=None):
+    """ Sample a random number from the probability distribution pdf
+    
+    This function returns a random value in the interval [xmin,xmax] distributed
+    according to the distribution pdf(x).
+    """
+    
+    if rng is None:
+        rng = simtools.get_random_state()
+        
+    x = xmin + rng.random_sample() * (xmax-xmin)
+    y = rng.random_sample()
+    
+    while y > pdf(x):
+        x = xmin + rng.random_sample() * (xmax-xmin)
+        y = rng.random_sample()
+    
+    return x
+    
 
 class FreeSpaceLossModel(QuantumErrorModel):
     """Model for photon loss on a free space channel
@@ -27,55 +44,27 @@ class FreeSpaceLossModel(QuantumErrorModel):
     
     Parameters
     ----------
+    W0 : float
+        Waist of the beam at the transmitter [m].
+    rx_aperture : float
+        Radius of the receiving telescope [m].
+    Cn2 : float
+        Index of refraction structure constant [m**(-2/3)].
+    wavelength : float
+        Wavelength of the radiation [m].
     rng : :obj:`~numpy.random.RandomState` or None, optional
         Random number generator to use. If ``None`` then
         :obj:`~netsquid.util.simtools.get_random_state` is used.
     
     """
-    def __init__(self, length, W0, rx_aperture, Cn2, wavelength, rng=None):
+    def __init__(self, W0, rx_aperture, Cn2, wavelength, rng=None):
         super().__init__()
         self.rng = rng if rng else simtools.get_random_state()
-        
-        # TODO: substitute kwargs with parameters of the model
-        # -> length could stay in kwargs, the other ones must be in the parameter of init
-        
-    def PDTC(self,T,**kwargs):
-        # TODO: put the initialization of these paramters in the __init__ function
-        z = kwargs['length']*1e3
-        W = kwargs['W0']*np.sqrt(1 + (z*kwargs['wavelength']/(np.pi*kwargs['W0']**2))**2)
-        X = (kwargs['rx_aperture']/W)**2
-        T0 = np.sqrt(1 - np.exp(-2*X))
-        sigma = np.sqrt(1.919 * kwargs['Cn2'] * kwargs['length']**3 * (2*kwargs['W0'])**(-1./3.))
-        l = 8 * X * np.exp(-4*X) * i1(4*X) / (1 - np.exp(-4*X)*i0(4*X)) / np.log( 2*T0**2/(1-np.exp(-4*X)*i0(4*X)))
-        R = kwargs['rx_aperture'] * np.log( 2*T0**2/(1-np.exp(-4*X)*i0(4*X)) )**(-1./l)
-        
-        return 2*R**2/(sigma**2*l*T) * (2*np.log(T0/T))**((2./l)-1) * np.exp( -R**2 * (2*np.log(T0/T))**(2./l) / (2*sigma**2) )
-    
-
-class FibreLossModel(QuantumErrorModel):
-    """Model for exponential photon loss on fibre optic channels.
-
-    Uses length of transmitting channel to sample an
-    exponential loss probability.
-
-    Parameters
-    ----------
-    p_loss_init : float, optional
-        Initial probability of losing a photon once it enters a channel.
-        e.g. due to frequency conversion.
-    p_loss_length : float, optional
-        Photon survival probability per channel length [dB/km].
-    rng : :obj:`~numpy.random.RandomState` or None, optional
-        Random number generator to use. If ``None`` then
-        :obj:`~netsquid.util.simtools.get_random_state` is used.
-
-    """
-    def __init__(self, p_loss_init=0.2, p_loss_length=0.25, rng=None):
-        super().__init__()
-        self.p_loss_init = p_loss_init
-        self.p_loss_length = p_loss_length
-        self.rng = rng if rng else simtools.get_random_state()
-        self.required_properties = ["length"]
+        self.W0 = W0
+        self.rx_aperture = rx_aperture
+        self.Cn2 = Cn2
+        self.wavelength = wavelength
+        self.required_properties = ['length']
 
     @property
     def rng(self):
@@ -89,27 +78,49 @@ class FibreLossModel(QuantumErrorModel):
         self.properties['rng'] = value
 
     @property
-    def p_loss_init(self):
-        """float: initial probability of losing a photon when it enters channel."""
-        return self.properties['p_loss_init']
+    def W0(self):
+        """float: beam waist at the transmitter [m]."""
+        return self.properties['W0']
 
-    @p_loss_init.setter
-    def p_loss_init(self, value):
-        if not 0 <= value <= 1:
-            raise ValueError
-        self.properties['p_loss_init'] = value
-
-    @property
-    def p_loss_length(self):
-        """float: photon survival probability per channel length [dB/km]."""
-        return self.properties['p_loss_length']
-
-    @p_loss_length.setter
-    def p_loss_length(self, value):
+    @W0.setter
+    def W0(self, value):
         if value < 0:
             raise ValueError
-        self.properties['p_loss_length'] = value
+        self.properties['W0'] = value
 
+    @property
+    def rx_aperture(self):
+        """float: radius of the receiving telescope [m]."""
+        return self.properties['rx_aperture']
+
+    @rx_aperture.setter
+    def rx_aperture(self, value):
+        if value < 0:
+            raise ValueError
+        self.properties['rx_aperture'] = value
+
+    @property
+    def Cn2(self):
+        """float: index of refraction structure constant [m**(-2/3)]."""
+        return self.properties['Cn2']
+
+    @Cn2.setter
+    def Cn2(self, value):
+        if value < 0:
+            raise ValueError
+        self.properties['Cn2'] = value
+
+    @property
+    def wavelength(self):
+        """float: wavelength of the radiation [m]."""
+        return self.properties['wavelength']
+
+    @wavelength.setter
+    def wavelength(self, value):
+        if value < 0:
+            raise ValueError
+        self.properties['wavelength'] = value
+            
     def error_operation(self, qubits, delta_time=0, **kwargs):
         """Error operation to apply to qubits.
 
@@ -124,16 +135,26 @@ class FibreLossModel(QuantumErrorModel):
         if 'channel' in kwargs:
             warn_deprecated("channel parameter is deprecated. "
                             "Pass length parameter directly instead.",
-                            key="FibreLossModel.compute_model.channel")
+                            key="FreeSpaceLossModel.compute_model.channel")
             kwargs['length'] = kwargs['channel'].properties["length"]
             del kwargs['channel']
-        #self.apply_loss(qubits, delta_time, **kwargs)
+            
+        # calculate the parameters used in the PDTC
+        z = kwargs['length']*1e3
+        W = self.W0*np.sqrt(1 + (z*self.wavelength/(np.pi*self.W0**2))**2)
+        X = (self.rx_aperture/W)**2
+        T0 = np.sqrt(1 - np.exp(-2*X))
+        sigma = np.sqrt(1.919 * self.Cn2 * kwargs['length']**3 * (2*self.W0)**(-1./3.))
+        l = 8 * X * np.exp(-4*X) * i1(4*X) / (1 - np.exp(-4*X)*i0(4*X)) / np.log( 2*T0**2/(1-np.exp(-4*X)*i0(4*X)))
+        R = self.rx_aperture * np.log( 2*T0**2/(1-np.exp(-4*X)*i0(4*X)) )**(-1./l)
+        # define the PDTC
+        PDTC = lambda T: 2*R**2/(sigma**2*l*T) * (2*np.log(T0/T))**((2./l)-1) * np.exp( -R**2 * (2*np.log(T0/T))**(2./l) / (2*sigma**2) )
+
         for idx, qubit in enumerate(qubits):
             if qubit is None:
                 continue
-            prob_loss = 1 - (1 - self.p_loss_init) * np.power(10, - kwargs['length'] * self.p_loss_length / 10)
+            # extract the value of the transmission coefficient
+            T = randomSample(PDTC,0,T0,self.rng)
+            # calculate the probability of losing the qubit
+            prob_loss = 1 - T**2
             self.lose_qubit(qubits, idx, prob_loss, rng=self.properties['rng'])
-
-    def prob_item_lost(self, item, delta_time=0, **kwargs):
-        # DEPRECATED
-        return 1 - (1 - self.p_loss_init) * np.power(10, - kwargs['length'] * self.p_loss_length / 10)
